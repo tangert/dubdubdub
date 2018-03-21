1006d2a60cf2db7a5c1e15f5bc7ab198f3850fe3
tangert <tyler@angert.com>
2018-03-21 18:30:54 -0400

    fixed bugs in k-means and added basic visualization

--- kMeans.playground/Contents.swift
+++ kMeans.playground/Contents.swift
@@ -1,4 +1,15 @@
 import SceneKit
+import PlaygroundSupport
+
+/*
+ Playground stuff
+ */
+let WIDTH = 500
+let HEIGHT = 500
+
+let container = UIView(frame: CGRect(x: 0, y: 0, width: WIDTH, height: HEIGHT))
+PlaygroundPage.current.liveView = container
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 protocol Graphable {
     var x: Float! { get set }
@@ -13,6 +24,8 @@ class Centroid: Graphable {
     var coordinates: (Float, Float)! {
         return (x, y)
     }
+    var allCoordinates = [(Float, Float)]()
+    var color: UIColor!
     
     init() {
         self.x = 0
@@ -32,6 +45,7 @@ class Centroid: Graphable {
 class DataPoint: Graphable {
     
     //Adjusts on each iteration
+    var allCentroids = [Centroid]()
     var previousCentroid = Centroid()
     var currentCentroid = Centroid()
     
@@ -60,6 +74,21 @@ class DataPoint: Graphable {
 
 
 // K-means functions
+func euclideanAverage(dataPoints: [DataPoint]) -> (Float, Float) {
+    var sumX: Float = 0
+    var sumY: Float = 0
+    
+    for point in dataPoints {
+        sumX += point.x
+        sumY += point.y
+    }
+    
+    let avgX = sumX/Float(dataPoints.count)
+    let avgY = sumY/Float(dataPoints.count)
+    
+    return (avgX, avgY)
+}
+
 func distance(point1: Graphable, point2: Graphable) -> Float {
     let yDist = point2.y - point1.y
     let xDist = point2.x - point1.x
@@ -78,24 +107,38 @@ func random(start: Int, end: Int) -> Int {
     return Int(arc4random_uniform(UInt32(b - a + 1))) + a
 }
 
+func generateRandomColor() -> UIColor {
+    let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
+    let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.75 // from 0.5 to 1.0 to stay away from white
+    let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.75 // from 0.5 to 1.0 to stay away from black
+    
+    return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+}
+
+func printInfo(point: DataPoint) {
+    print("point: \(point.coordinates!)")
+    print("point current centroid: \(point.currentCentroid.coordinates!) | previous centroid: \(point.previousCentroid.coordinates!)\n")
+}
+
 
 ////////////////////////////////////
 // MARK: K MEANS IMPLEMENTATION
 ////////////////////////////////////
 
 // Set k
-let k = 5
+let k = 3
 
 // Create the data points
 var dataPoints = [DataPoint]()
 
 let start = 0
-let end = 50
+let end = 100
 
 //Form the data points in clusters
-for num in start..<end {
-    let x = Float(random(start: num, end: end))
-    let y = Float(random(start: num, end: end))
+//Form k clusters
+for _ in start..<end {
+    let x = Float(random(start: start, end: end))
+    let y = Float(random(start: start, end: end))
     let point = DataPoint(x: x, y: y)
     dataPoints.append(point)
 }
@@ -105,10 +148,12 @@ for num in start..<end {
 var centroids = [Centroid]()
 
 // Initialize random centroid locations
-for _ in 0..<k {
-    let x = Float(random(start: start, end: end))
-    let y = Float(random(start: start, end: end))
+for i in 1...k {
+    let x = Float(random(start: start, end: end/i))
+    let y = Float(random(start: start, end: end/i))
     let centroid = Centroid(x: x, y: y)
+    centroid.allCoordinates.append((x, y))
+    centroid.color = generateRandomColor()
     centroids.append(centroid)
 }
 
@@ -123,6 +168,7 @@ while !converged {
     // Checks convergence by comparing the previous and current centroids of all data points
     var allSame = true
     
+    // Assign the points to each centroid
     for point in dataPoints {
         
         var minDist = Float.greatestFiniteMagnitude
@@ -138,15 +184,28 @@ while !converged {
         
         point.previousCentroid = point.currentCentroid
         point.currentCentroid = closestCentroid
+        point.allCentroids.append(closestCentroid)
         
         if point.currentCentroid.coordinates! != point.previousCentroid.coordinates! {
             allSame = false
         }
+    }
+    
+    // Get the new location for each centroid
+    for centroid in centroids {
         
-        print("point: \(point.coordinates!)")
-        print("min dist: \(minDist) | centroid: \(closestCentroid.coordinates!)")
-        print("point current centroid: \(point.currentCentroid.coordinates!) | previous centroid: \(point.previousCentroid.coordinates!)\n")
-
+        var currentPoints = [DataPoint]()
+        
+        for data in dataPoints {
+            if data.currentCentroid.coordinates == centroid.coordinates {
+                currentPoints.append(data)
+            }
+        }
+        
+        let newLocation = euclideanAverage(dataPoints: currentPoints)
+        centroid.allCoordinates.append(newLocation)
+        centroid.x = newLocation.0
+        centroid.y = newLocation.1
     }
     
     if allSame {
@@ -158,4 +217,32 @@ while !converged {
     
 }
 
+// Plot the centroids
+for centroid in centroids {
+    for i in 1...centroid.allCoordinates.count {
+        
+        let coord = centroid.allCoordinates[i-1]
+        let scaledX = (coord.0)/Float(end) * Float(WIDTH)
+        let scaledY = (coord.1)/Float(end) * Float(HEIGHT)
+        
+        let dataView = UIView(frame: CGRect(x: Double(scaledX), y: Double(scaledY), width: 15, height: 15))
+        print ("i: \(i) | count: \(centroid.allCoordinates.count)")
+        let opacity = Double(i)/Double(centroid.allCoordinates.count)
+        print("OPACITY: \(opacity)")
+        
+        dataView.backgroundColor = UIColor.white.withAlphaComponent(CGFloat(opacity))
+        dataView.layer.cornerRadius = 10
+        container.addSubview(dataView)
+    }
+}
 
+// Plot the data
+for data in dataPoints {
+    let scaledX = (data.x)/Float(end) * Float(WIDTH)
+    let scaledY = (data.y)/Float(end) * Float(HEIGHT)
+    
+    let dataView = UIView(frame: CGRect(x: Double(scaledX), y: Double(scaledY), width: 10, height: 10))
+    dataView.backgroundColor = data.currentCentroid.color.withAlphaComponent(0.75)
+    dataView.layer.cornerRadius = 7.5
+    container.addSubview(dataView)
+}
